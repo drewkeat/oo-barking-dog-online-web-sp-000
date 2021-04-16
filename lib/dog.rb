1380bf68db9a0a4da49d01b5373a16ffc8fcaf71
Drew Keat <thedrewkeat@gmail.com>
2021-04-16 15:05:01 -0600

    commit tree 759807de2d4ea882b9e230a95af4ea0e4b5f10b8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Your code goes here!
+class Dog
+    def name=(text)
+        @dog_name = text
+    end
+
+    def name
+        @dog_name
+    end
+
+    def bark
+        puts "woof!"
+    end
+end
